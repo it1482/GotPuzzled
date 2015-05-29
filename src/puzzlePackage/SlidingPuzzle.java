dbe939b05fea0ca2c4b8172019bdfdd0b4e8fd22
Giorgos Angelopoulos <it1482@uom.edu.gr>
2015-05-29 20:29:35 +0300

    Sliding puzzle Ver 1.1

--- src/puzzlePackage/SlidingPuzzle.java
+++ src/puzzlePackage/SlidingPuzzle.java
@@ -9,13 +9,15 @@ import java.awt.Insets;
 import java.awt.MediaTracker;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -24,11 +26,14 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 
 	
 	
-
+	int[] thesis = new int[9];
+	JButton help = new JButton("Help");
 	Image image;
 	ImageIcon icon;
 	JButton buttons[],blankButton,newGame;
-	JPanel grid,spots[];
+	//ArrayList<JPanel> rightspots = new ArrayList<JPanel>();
+	//ArrayList<JPanel> tempspots = new ArrayList<JPanel>();
+	JPanel grid,spots[],tempspots[],rightspots[];
 	int currentBlankSpot,movecounter=0;
 	JLabel moves_label,time_label;
 	
@@ -37,8 +42,11 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 	int size_pleuras;
 	public SlidingPuzzle(String name, Image image, int partsNumber) {
 		super(name, image, partsNumber);
+		//gia na perastei i photo
+		//this.image = image;
 		
 		//Pleura tou puzzle
+		
 		size_pleuras=partsNumber;
 		setSize(400,300);
 		
@@ -50,7 +58,9 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 		setupImage();
 		
 		add(grid);
-		
+		add(help);
+		MouseHandler handler = new MouseHandler();
+		help.addMouseListener(handler);
 		moves_label = new JLabel("Moves: "+Integer.toString(movecounter));
 		add(moves_label,BorderLayout.SOUTH);
 		
@@ -65,6 +75,7 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 			spots[i] = new JPanel(new BorderLayout());
 			grid.add(spots[i]);
 		}
+		
 	}
 	
 	public void setupImage(){
@@ -88,7 +99,7 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 		try{
 			tracker.waitForAll();
 			image = ImageIO.read(new File("images/pic.jpg"));
-			image=image.getScaledInstance(500,500, Image.SCALE_SMOOTH);
+			image=image.getScaledInstance(600,600, Image.SCALE_SMOOTH);
 		} catch (Exception e){
 			
 		
@@ -102,43 +113,50 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 		int height=image.getHeight(this);
 		
 		int count = 0;
-		for(int i=0;i<size_pleuras;i++)
-			for(int j=0;j<size_pleuras;j++){
+		for(int j=0;j<size_pleuras;j++)
+			for(int i=0;i<size_pleuras;i++){
 				BufferedImage window = bimage.getSubimage(i*width/size_pleuras,
 															j*height/size_pleuras,
 															width/size_pleuras,
 															height/size_pleuras);
-				setupButton(count++,window);
+				setupButton(count,window);
+				count++;
 			}
 		spots[spots.length-1].add(blankButton);
 		currentBlankSpot = spots.length-1;
+		rightspots[spots.length-1]=spots[spots.length-1];
 	}
 	
 	public void setupButton(int id,Image img){
-		buttons[id] = new JButton(new ImageIcon(img));
+		buttons[id] = new JButton(Integer.toString(id),new ImageIcon(img));
 		buttons[id].addActionListener(this);
 		buttons[id].setMargin(new Insets(0,0,0,0));
 		buttons[id].setContentAreaFilled(false);
+		//thesis[id]=id;
 		spots[id].add(buttons[id],BorderLayout.CENTER);
+		rightspots[id]=spots[id];
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		Object src = e.getSource();
-		for(int i=0;i<buttons.length;i++){
-			if(buttons[i] == src){
-				changeSpots(i);
-				return;
-			}
-				 
+		if(src == help){
 			
+			
+		}else{
+			for(int i=0;i<buttons.length;i++){
+				if(buttons[i] == src){
+					changeSpots(i);
+					return;
+				}
+			}
 		}
 	}
 	
 	public void changeSpots(int i){
-		if(currentBlankSpot-1 == i || currentBlankSpot -3 == i ||
-				currentBlankSpot +1 == i || currentBlankSpot + 3 == i){
+		if(currentBlankSpot-1 == i || currentBlankSpot -size_pleuras == i ||
+				currentBlankSpot +1 == i || currentBlankSpot + size_pleuras == i){
 			movecounter++;
 			moves_label.setText("Moves : "+Integer.toString(movecounter));
 			
@@ -153,11 +171,52 @@ public class SlidingPuzzle extends Puzzle implements ActionListener{
 			currentBlankSpot = i;
 			
 			repaint();
+				
 			return;
 			
 		}
+		
+		
 	}
 	
-	
+	class MouseHandler implements MouseListener{
+		
+		
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			for(int i=0;i<spots.length-1;i++){
+				tempspots[i]=spots[i];
+				spots[i].removeAll();
+				spots[i]=tempspots[i];
+				
+			}			
+			repaint();
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 
 }
