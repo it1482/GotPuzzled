1b9d2dab7561e5a971e6e91ce2ec7ecb13a13bbd
it11168 <it11168@uom.edu.gr>
2015-05-30 18:06:49 +0300

    Charis made up the first components for the GUI mainMenuPanel with 4 button (ladder, custom and exit working) playPanel (3 basic buttons open ladder and customGame panels) ladderChallengePanel (panel with jlist to be filled) custonmGamePanel (panel with jlist to be filled) back buttons working see ya tomorrow

--- src/guiPackage/MainGUI.java
+++ src/guiPackage/MainGUI.java
@@ -4,7 +4,6 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
-
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -12,10 +11,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+
 import java.awt.Font;
 import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.border.LineBorder;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
 
 public class MainGUI {
 	
@@ -27,7 +32,13 @@ public class MainGUI {
 
 	
 	// kentriko panel
-	private JPanel mainMenu;
+	private JPanel mainMenuPanel;
+	
+	private JPanel playPanel; 
+	
+	private JPanel ladderChallengePanel;
+	
+	private JPanel customGamePanel;
 	
 	
 	
@@ -69,7 +80,7 @@ public class MainGUI {
 		frmGotPuzzled = new JFrame();
 		frmGotPuzzled.getContentPane().setBackground(new Color(240, 255, 255));
 		frmGotPuzzled.setTitle("Got Puzzled 1.0");
-		frmGotPuzzled.setBounds(100, 100, 703, 488);
+		frmGotPuzzled.setBounds(100, 100, 703, 465);
 		frmGotPuzzled.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
@@ -79,6 +90,11 @@ public class MainGUI {
 		menuBar.add(mnNewMenu);
 		
 		JMenuItem mntmNewMenuItem = new JMenuItem("Exit");
+		mntmNewMenuItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);				
+			}
+		});
 		mntmNewMenuItem.setIcon(new ImageIcon(MainGUI.class.getResource("/guiPackage/img/exit.png")));
 		mnNewMenu.add(mntmNewMenuItem);
 		
@@ -90,38 +106,179 @@ public class MainGUI {
 		mnAbout.add(mntmAboutGotPuzzled);
 		frmGotPuzzled.getContentPane().setLayout(null);
 		
-		mainMenu = new JPanel();
-		mainMenu.setBorder(new LineBorder(new Color(0, 0, 0)));
-		mainMenu.setBackground(new Color(224, 255, 255));
-		mainMenu.setBounds(160, 12, 357, 402);
-		frmGotPuzzled.getContentPane().add(mainMenu);
-		mainMenu.setLayout(null);
+		mainMenuPanel = new JPanel();
+		mainMenuPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		mainMenuPanel.setBackground(new Color(224, 255, 255));
+		mainMenuPanel.setBounds(12, 12, 663, 379);
+		frmGotPuzzled.getContentPane().add(mainMenuPanel);
+		mainMenuPanel.setLayout(null);
 		
-		JLabel title = new JLabel("Got Puzzled!");
-		title.setBounds(0, 0, 357, 31);
-		title.setFont(new Font("Segoe UI Black", Font.PLAIN, 26));
-		title.setHorizontalAlignment(SwingConstants.CENTER);
-		mainMenu.add(title);
+		JLabel mainTitle = new JLabel("Got Puzzled!");
+		mainTitle.setBounds(139, 12, 357, 31);
+		mainTitle.setFont(new Font("Segoe UI Black", Font.PLAIN, 26));
+		mainTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		mainMenuPanel.add(mainTitle);
 		
 		JButton playButton = new JButton("Play!");
+		playButton.setBounds(225, 71, 177, 64);
+		playButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				mainMenuPanel.setVisible(false);
+				playPanel.setVisible(true);
+			}
+		});
 		playButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
-		playButton.setBounds(86, 59, 177, 64);
-		mainMenu.add(playButton);
+		mainMenuPanel.add(playButton);
 		
 		JButton editorButton = new JButton("Editor");
+		editorButton.setBounds(225, 147, 177, 64);
 		editorButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
-		editorButton.setBounds(86, 135, 177, 64);
-		mainMenu.add(editorButton);
+		mainMenuPanel.add(editorButton);
 		
 		JButton optionsButton = new JButton("Options");
+		optionsButton.setBounds(225, 223, 177, 64);
 		optionsButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
-		optionsButton.setBounds(86, 211, 177, 64);
-		mainMenu.add(optionsButton);
+		mainMenuPanel.add(optionsButton);
 		
 		JButton exitButton = new JButton("Exit");
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		exitButton.setBounds(225, 299, 177, 64);
 		exitButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
-		exitButton.setBounds(86, 287, 177, 64);
-		mainMenu.add(exitButton);
+		mainMenuPanel.add(exitButton);
+		
+		playPanel = new JPanel();
+		playPanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		playPanel.setBackground(new Color(224, 255, 255));
+		playPanel.setBounds(12, 12, 663, 379);
+		frmGotPuzzled.getContentPane().add(playPanel);
+		// creates the playPanel and set it non visible
+		// playPanel will become visible when the user will press the playButton
+		playPanel.setVisible(false);
+		
+		JButton returnToMainPanelButton = new JButton("back");
+		returnToMainPanelButton.setBounds(27, 280, 80, 80);
+		returnToMainPanelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				playPanel.setVisible(false);
+				mainMenuPanel.setVisible(true);
+			}
+		});
+		playPanel.setLayout(null);
+		
+		JLabel playTitle = new JLabel("Play!");
+		playTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		playTitle.setFont(new Font("Segoe UI Black", Font.PLAIN, 26));
+		playTitle.setBounds(139, 12, 357, 31);
+		playPanel.add(playTitle);
+		returnToMainPanelButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 17));
+		playPanel.add(returnToMainPanelButton);
+		
+		JButton ladderChallengeButton = new JButton("Ladder Challenge");
+		ladderChallengeButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				ladderChallengePanel.setVisible(true);
+				playPanel.setVisible(false);
+			}
+		});
+		ladderChallengeButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 16));
+		ladderChallengeButton.setBounds(225, 71, 175, 62);
+		playPanel.add(ladderChallengeButton);
+		
+		JButton customGameButton = new JButton("Custom Game");
+		customGameButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				customGamePanel.setVisible(true);
+				playPanel.setVisible(false);
+			}
+		});
+		customGameButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 16));
+		customGameButton.setBounds(225, 147, 175, 62);
+		playPanel.add(customGameButton);
+		
+		ladderChallengePanel = new JPanel();
+		ladderChallengePanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		ladderChallengePanel.setBackground(new Color(224, 255, 255));
+		ladderChallengePanel.setBounds(12, 12, 663, 379);
+		frmGotPuzzled.getContentPane().add(ladderChallengePanel);
+		ladderChallengePanel.setVisible(false);
+		
+		
+		JButton returnToPlayPanelButton = new JButton("back");
+		returnToPlayPanelButton.setBounds(27, 280, 80, 80);
+		returnToPlayPanelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ladderChallengePanel.setVisible(false);
+				playPanel.setVisible(true);
+			}
+		});
+		ladderChallengePanel.setLayout(null);
+		returnToPlayPanelButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 17));
+		ladderChallengePanel.add(returnToPlayPanelButton);
+		
+		JLabel ladderTitle = new JLabel("Ladder Challenge!");
+		ladderTitle.setBounds(139, 12, 357, 31);
+		ladderTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		ladderTitle.setFont(new Font("Segoe UI Black", Font.PLAIN, 26));
+		ladderChallengePanel.add(ladderTitle);
+		
+		JLabel ladderSubTitle = new JLabel("Choose you destiny!");
+		ladderSubTitle.setBounds(139, 45, 357, 31);
+		ladderSubTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		ladderSubTitle.setFont(new Font("Segoe UI", Font.PLAIN, 23));
+		ladderChallengePanel.add(ladderSubTitle);
+		
+		JList laddersJList = new JList();
+		laddersJList.setBorder(new LineBorder(new Color(0, 0, 0)));
+		laddersJList.setBounds(165, 120, 300, 150);
+		ladderChallengePanel.add(laddersJList);
+		
+		JButton startLadderButton = new JButton("Start Ladder!");
+		startLadderButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
+		startLadderButton.setBounds(240, 300, 190, 50);
+		ladderChallengePanel.add(startLadderButton);
+		
+		customGamePanel = new JPanel();
+		customGamePanel.setBounds(12, 12, 663, 379);
+		frmGotPuzzled.getContentPane().add(customGamePanel);
+		customGamePanel.setBackground(new Color(224, 255, 255));
+		customGamePanel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		customGamePanel.setLayout(null);
+		
+		JButton returnToPlayPanelButton2 = new JButton("back");
+		returnToPlayPanelButton2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				customGamePanel.setVisible(false);
+				playPanel.setVisible(true);
+			}
+		});
+		
+		JLabel customGameTitle = new JLabel("Custom Game!");
+		customGameTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		customGameTitle.setFont(new Font("Segoe UI Black", Font.PLAIN, 26));
+		customGameTitle.setBounds(139, 12, 357, 31);
+		customGamePanel.add(customGameTitle);
+		
+		JList customGameJList = new JList();
+		customGameJList.setBorder(new LineBorder(new Color(0, 0, 0)));
+		customGameJList.setBounds(165, 120, 300, 150);
+		customGamePanel.add(customGameJList);
+		returnToPlayPanelButton2.setFont(new Font("Segoe UI Black", Font.PLAIN, 17));
+		returnToPlayPanelButton2.setBounds(27, 280, 80, 80);
+		customGamePanel.add(returnToPlayPanelButton2);
+		
+		JButton startCustomGameButton = new JButton("Start Game!");
+		startCustomGameButton.setFont(new Font("Segoe UI Black", Font.PLAIN, 20));
+		startCustomGameButton.setBounds(240, 300, 190, 50);
+		customGamePanel.add(startCustomGameButton);
+		customGamePanel.setVisible(false);
+		
+	
+		
 		
 		
 	}
